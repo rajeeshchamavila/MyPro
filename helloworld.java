178a50163867b146559ea6135d82e90ab52075f3
rajeeshchamavila <152488816+rajeeshchamavila@users.noreply.github.com>
2023-12-22 15:35:32 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!\n"); 
-        System.out.println("Thejus Kili Annan is here"); 
+        System.out.println("Thejus Kili Annan is here from school"); 
     }
 }
